2d1fdec53f7763c7225fc059a7fec2af7378b861
Ayhan <ayhan-games@hotmail.com>
2024-12-13 01:51:58 +0300

    home updated

---
+++ frontend/src/components/HomeFriend.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HomeFriend = () => {
+  return (
+    <div className="w-full h-auto text-gray-400 font-semibold flex p-6 gap-5 items-center border-t border-gray-600 ">
+                <img
+                  src="https://m.media-amazon.com/images/I/61GU80tkXwL._AC_UF894,1000_QL80_.jpg"
+                  className="w-10 h-10 rounded-full"
+                />
+                <h3 className="text-xl ">Ayhan Taha Pastutmaz</h3>
+              </div>
+  )
+}
+
+export default HomeFriend
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,24 +1,39 @@
-
-import Menu from '../components/Menu'
-import TopBar from '../components/TopBar'
+import HomeFriend from "../components/HomeFriend";
+import Menu from "../components/Menu";
+import TopBar from "../components/TopBar";
 import { IoMdSearch } from "react-icons/io";
 
 const Home = () => {
   return (
-    <div className='w-full flex bg-[#313338]'>
+    <div className="w-full flex bg-[#313338]">
       <Menu />
-      <div className='flex flex-col w-full'>
-      <TopBar />
-      <div className='w-[70%] h-12 flex  items-center m-3 relative '>
-        <div className='w-full h-[70%] relative'>
-        <input className='w-full h-full bg-[#1E1F22] rounded-lg pl-2 outline-none text-white' placeholder='Search' type="text" />
-        <IoMdSearch className='absolute text-2xl text-gray-400 right-0 bottom-1 mr-1' />
+      <div className="flex flex-col w-full">
+        <TopBar />
+        <div className="w-[80%] border-r  border-x-gray-600 h-[88%]">
+          <div className="w-full h-12 flex  items-center  relative ">
+            <div className="w-full h-[70%] relative flex justify-center">
+              <input
+                className="w-[95%] h-full bg-[#1E1F22] rounded-lg pl-2 outline-none text-white"
+                placeholder="Search"
+                type="text"
+              />
+              <IoMdSearch className="absolute text-2xl text-gray-400 right-6 bottom-1 mr-1" />
+            </div>
+          </div>
+          <div className="w-full h-full">
+            <div className="w-full h-auto text-gray-400 px-7 py-3 font-bold ">
+              <h3>ONLINE - 2</h3>
+            </div>
+            <div className="w-full h-auto p-3">
+              <HomeFriend />
+              <HomeFriend />
+              <HomeFriend />
+            </div>
+          </div>
         </div>
       </div>
-      </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
