e7c100fd486acc672fc5398812540a741302d261
Ayhan <ayhan-games@hotmail.com>
2024-12-13 18:23:25 +0300

    home and homefriend updated

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,10 +2,11 @@ import HomeFriend from "../components/HomeFriend";
 import Menu from "../components/Menu";
 import TopBar from "../components/TopBar";
 import { IoMdSearch } from "react-icons/io";
+import "../index.css"
 
 const Home = () => {
   return (
-    <div className="w-full flex bg-[#313338]">
+    <div className="w-full h-screen flex bg-[#313338]">
       <Menu />
       <div className="flex flex-col w-full">
         <TopBar />
@@ -24,10 +25,16 @@ const Home = () => {
             <div className="w-full h-auto text-gray-400 px-7 py-3 font-bold ">
               <h3>ONLINE - 2</h3>
             </div>
-            <div className="w-full h-auto p-3">
+            <div className="w-full h-[calc(100%-60px)] p-3  overflow-y-auto  custom-scrollbar ">
               <HomeFriend />
               <HomeFriend />
               <HomeFriend />
+              <HomeFriend />
+              <HomeFriend />
+              <HomeFriend />
+              <HomeFriend />
+              <HomeFriend />
+              
             </div>
           </div>
         </div>
