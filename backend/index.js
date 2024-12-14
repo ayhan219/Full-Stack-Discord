0ade0b744fffc7af60d337f0538896d63330efc1
Ayhan <ayhan-games@hotmail.com>
2024-12-14 22:05:37 +0300

    index.js updated

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require("mongoose")
 const dotenv = require("dotenv");
 const cors = require("cors");
+const UserRoutes = require("./router/UserRouter");
 dotenv.config();
 
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth",UserRoutes);
 
 
 const connectToDB = async()=>{
