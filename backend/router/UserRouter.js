0ec41fe2192a54557f76f4978505b0808b31af18
Ayhan <ayhan-games@hotmail.com>
2024-12-14 22:04:16 +0300

    UserRouter and UserController added and updated

---
+++ backend/router/UserRouter.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+const {signup} = require("../controller/UserController");
+
+
+router.post("/signup",signup)
+
+
+module.exports = router;
