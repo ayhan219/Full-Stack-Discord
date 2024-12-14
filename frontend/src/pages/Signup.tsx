853d644845851d857c18e9372763df91a4aa21af
Ayhan <ayhan-games@hotmail.com>
2024-12-14 03:08:17 +0300

    signup updated

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,6 +1,79 @@
+import { useState } from "react";
 import dcbackgroung from "../assets/discordback.png";
 
 const Signup = () => {
+  const [email, setEmail] = useState<string>("");
+  const [isEmailValid, setIsEmailValid] = useState<boolean>(true);
+
+  const [displayName,setDisplayName] = useState<string>("");
+  const [isDisplayNameValid,setIsDisplayNameValid] = useState<boolean>(true);
+
+  const [username,setUsername] = useState<string>("");
+  const [isUsernameValid,setIsUsernameValid] = useState<boolean>(true);
+
+  const [password, setPassword] = useState<string>("");
+  const [isPasswordValid, setIsPasswordValid] = useState<boolean>(true);
+
+  const [rePassword,setRePassword] = useState<string>("");
+  const [isPasswordMatching,setIsPasswordMatching] = useState<boolean>(false);
+
+  const [isTermsChecked, setIsTermsChecked] = useState<boolean>(false);
+
+
+
+  const handleEmail = (emailInput: string) => {
+    setEmail(emailInput);
+    const hasValidInput =
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(emailInput);
+    setIsEmailValid(hasValidInput);
+  };
+
+  const handleDisplayName = (displayNameInput: string) => {
+    setDisplayName(displayNameInput);
+    const hasValidInput = displayNameInput.length >= 6;
+    setIsDisplayNameValid(hasValidInput);
+  };
+
+  const handleUsername = (usernameInput:string)=>{
+    setUsername(usernameInput);
+    const hasValidInput1 = username.length >=6;
+    const hasValidInput2 = /\d/.test(usernameInput);
+    setIsUsernameValid(hasValidInput1 && hasValidInput2);
+  }
+
+  const handlePassword = (passwordInput: string) => {
+    setPassword(passwordInput);
+    const hasValidPassword =
+      /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&*!])[A-Za-z\d@#$%^&*!]{8,}$/.test(
+        passwordInput
+      );
+    setIsPasswordValid(hasValidPassword);
+  };
+
+  const handleRePassword = (rePasswordInput:string)=>{
+    setRePassword(rePasswordInput);
+    if(rePasswordInput === password){
+        setIsPasswordMatching(true);
+    }
+    else{
+        setIsPasswordMatching(false);
+    }
+  }
+  
+
+  const handleCheckboxChange = () => {
+    setIsTermsChecked(!isTermsChecked); 
+  };
+
+
+  const handleSubmit = ()=>{
+    if(isEmailValid && isDisplayNameValid && isUsernameValid && isPasswordValid && isPasswordMatching && isTermsChecked ){
+        alert("submit successfull!")
+    }else{
+        alert("error")
+    }
+  }
+
   return (
     <div
       className="w-full h-screen bg-cover bg-center flex justify-center items-center"
@@ -11,58 +84,64 @@ const Signup = () => {
           <h3>Create an account</h3>
         </div>
 
-       
         <div className="w-full h-auto">
           <div className="w-full h-auto text-white font-medium flex flex-col p-4">
             <label className="px-7 text-sm">EMAIL</label>
             <div className="w-full h-auto flex items-center justify-center">
               <input
-                className="w-[90%] h-10 rounded-sm bg-[#1E1F23] outline-none text-white "
+                className={`w-[90%] h-10 rounded-sm bg-[#1E1F23] outline-none text-white ${!isEmailValid && email!=="" &&  "border border-red-600"} `}
                 type="email"
+                onChange={(e)=>handleEmail(e.target.value)}
+                value={email}
               />
             </div>
           </div>
 
-          
           <div className="w-full h-auto text-white font-medium flex flex-col p-4">
             <label className="px-7 text-sm">DISPLAY NAME</label>
             <div className="w-full h-auto flex items-center justify-center">
               <input
-                className="w-[90%] h-10 rounded-sm bg-[#1E1F23] outline-none text-white"
+                className={`w-[90%] h-10 rounded-sm bg-[#1E1F23] outline-none text-white ${!isDisplayNameValid && displayName!=="" && "border border-red-600"}`}
                 type="text"
+                onChange={(e)=>handleDisplayName(e.target.value)}
+                value={displayName}
               />
             </div>
           </div>
 
-          
           <div className="w-full h-auto text-white font-medium flex flex-col p-4">
             <label className="px-7 text-sm">USERNAME</label>
             <div className="w-full h-auto flex items-center justify-center">
               <input
-                className="w-[90%] h-10 rounded-sm bg-[#1E1F23] outline-none text-white "
+                className={`w-[90%] h-10 rounded-sm bg-[#1E1F23] outline-none text-white ${!isUsernameValid && username !=="" && "border border-red-600"}`}
                 type="text"
+                onChange={(e)=>handleUsername(e.target.value)}
+                value={username}
               />
             </div>
           </div>
 
-         
           <div className="w-full h-auto text-white font-medium flex flex-col p-4">
             <label className="px-7 text-sm">PASSWORD</label>
             <div className="w-full h-auto flex items-center justify-center">
               <input
-                className="w-[90%] h-10 rounded-sm bg-[#1E1F23] outline-none text-white "
+                className={`w-[90%] h-10 rounded-sm bg-[#1E1F23] outline-none text-white ${!isPasswordValid && password!=="" && "border border-red-600"}`}
                 type="password"
+                onChange={(e)=>handlePassword(e.target.value)}
+                value={password}
               />
             </div>
           </div>
 
-          
           <div className="w-full h-auto text-white font-medium flex flex-col p-4">
             <label className="px-7 text-sm">CONFIRM PASSWORD</label>
             <div className="w-full h-auto flex items-center justify-center">
               <input
-                className="w-[90%] h-10 rounded-sm bg-[#1E1F23] outline-none text-white "
+                className={`w-[90%] h-10 rounded-sm bg-[#1E1F23] outline-none text-white ${!isPasswordMatching && rePassword!=="" && "border border-red-600"} `}
                 type="password"
+                onChange={(e)=>handleRePassword(e.target.value)}
+                value={rePassword}
+                
               />
             </div>
           </div>
@@ -72,6 +151,8 @@ const Signup = () => {
                 type="checkbox"
                 className="hidden peer"
                 id="discord-checkbox"
+                checked={isTermsChecked} 
+                onChange={handleCheckboxChange} 
               />
               <span
                 className="w-6 h-6 border-2 border-gray-500 rounded-sm flex items-center justify-center 
@@ -105,14 +186,12 @@ const Signup = () => {
             </label>
           </div>
 
-          
           <div className="w-full h-auto flex justify-center py-4">
-            <button className="w-[90%] h-12 bg-blue-500 text-white rounded-sm hover:bg-blue-600 transition duration-300">
+            <button onClick={()=>handleSubmit()} className="w-[90%] h-12 bg-blue-500 text-white rounded-sm hover:bg-blue-600 transition duration-300">
               Sign Up
             </button>
           </div>
 
-         
           <div className="w-full h-auto text-center mt-4 text-sm text-gray-400">
             <p>
               Already have an account?{" "}
@@ -122,7 +201,6 @@ const Signup = () => {
             </p>
           </div>
         </div>
-        
       </div>
     </div>
   );
