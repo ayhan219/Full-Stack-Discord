71a255e4afc4a44a9bcb1720457c69f30ffbb3b6
Ayhan <ayhan-games@hotmail.com>
2024-12-15 21:57:01 +0300

    channelcontroller updated

--- backend/controller/ChannelController.js
+++ backend/controller/ChannelController.js
@@ -1,10 +1,38 @@
 const mongoose = require("mongoose");
 const User = require("../model/User")
+const Channel = require("../model/Channel")
 
 
 
 const createChannel = async(req,res)=>{
-    
+    const {userId,channelName} = req.body;
+
+    if(!userId){
+        return res.status(200).json({message:"user not authenticated"})
+    }
+    try {
+
+        const findUser = await User.findById(userId);
+
+        if(!findUser){
+            return res.status(400).json({message:"user not found"})
+        }
+        
+        const newChannel = new Channel({
+            channelName:channelName
+        })
+        findUser.ownChannel.push(newChannel._id);
+        await findUser.save();
+        await newChannel.save();
+
+        return res.status(201).json({
+            message: "Channel created successfully",
+            channel: newChannel,
+          });
+
+    } catch (error) {
+        return res.status(500).json({message:"server error"})
+    }
 }
 
 module.exports ={
