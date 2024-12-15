4caaf549f7174c0efc3bafdbfafe5598ff86e0c3
Ayhan <ayhan-games@hotmail.com>
2024-12-15 03:18:22 +0300

    chatrightarea and hatarea updated

--- frontend/src/components/ChatRightArea.tsx
+++ frontend/src/components/ChatRightArea.tsx
@@ -3,7 +3,7 @@ import ChannelMember from './ChannelMember'
 
 const ChatRightArea = () => {
   return (
-    <div className='w-[20%] h-full bg-[#2B2D31]'>
+    <div className='w-[15%] h-full bg-[#2B2D31]'>
         <div className='w-full h-10 text-gray-400 p-4 text-xl font-semibold'>
             <h3>Members - 3</h3>
         </div>
