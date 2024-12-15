6547c98731ba410a24ed1fcd74434f463db276af
Ayhan <ayhan-games@hotmail.com>
2024-12-15 21:20:12 +0300

    channel model updated

--- backend/model/Channel.js
+++ backend/model/Channel.js
@@ -1,1 +1,19 @@
 const mongoose = require("mongoose");
+
+
+const ChannelSchema = new mongoose.Schema({
+    channelName:{
+        type:String,
+        required:true
+    },
+    chatChannel:{
+        type:String,
+        default:null
+    },
+    voiceChannel:{
+        type:String,
+        default:null
+    }
+})
+
+module.exports = mongoose.model("Channel",ChannelSchema);
