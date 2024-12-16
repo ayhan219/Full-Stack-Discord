706d3c1cb58c8e9fa486c8df0cdd82c96a7e9db9
Ayhan <ayhan-games@hotmail.com>
2024-12-16 19:07:02 +0300

    chatright updated for showing members

--- frontend/src/components/ChatRightArea.tsx
+++ frontend/src/components/ChatRightArea.tsx
@@ -8,7 +8,7 @@ const ChatRightArea = () => {
   return (
     <div className='w-[15%] h-full bg-[#2B2D31]'>
         <div className='w-full h-10 text-gray-400 p-4 text-xl font-semibold'>
-            <h3>Members - 3</h3>
+            <h3>Members - {singleChannel?.channelUsers.length}</h3>
         </div>
         <div className='w-full h-auto pt-3'>
             {
