d40f0492df89da6b9388e93f5f75521b9e3b924c
ayhan219 <ayhan-games@hotmail.com>
2024-12-17 12:20:01 +0300

    usercontroller and userrouter updated

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -1,112 +1,126 @@
 const mongoose = require("mongoose");
-const User = require("../model/User")
+const User = require("../model/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-
-
-const signup = async(req,res)=>{
-    const {email,displayName,username,password} = req.body;
-    try {
-        if(!email || !displayName || !username || !password){
-            return res.status(400).json({message:"provide all area"})
-        }
-
-        const existingUser = await User.findOne({email});
-        if(existingUser){
-            return res.status(400).json({message:"user exist!"})
-        }
-
-        const hashedPW = await bcrypt.hash(password,10);
-        
-        const newUser = new User({
-            email,
-            displayName,
-            username,
-            password:hashedPW
-        })
-        await newUser.save();
-
-        return res.status(200).json({message:"signup successfull!"})
-    } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message });
+const signup = async (req, res) => {
+  const { email, displayName, username, password } = req.body;
+  try {
+    if (!email || !displayName || !username || !password) {
+      return res.status(400).json({ message: "provide all area" });
     }
 
-}
-
-
-const login = async(req,res)=>{
-    const {email,password} = req.body;
-
-    if(!email || !password){
-        return res.status(400).json({message:"provide all area"})
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "user exist!" });
     }
-    try {
-        
-        const findUser = await User.findOne({email});
-        if(!findUser){
-            return res.status(400).json({message:"user not found"})
-        }
-        const isPasswordMatch = await bcrypt.compare(password,findUser.password);
-        if(!isPasswordMatch){
-            return res.status(400).json({message:"password doesn't match"})
-        }
 
-        const token = jwt.sign({
-            userId:findUser._id,
-            email: findUser.email,
-            displayName:findUser.displayName,
-            username: findUser.username,
-        },
-        process.env.JWT_SECRET,{
-            expiresIn:"1h"
-        }
+    const hashedPW = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      email,
+      displayName,
+      username,
+      password: hashedPW,
+    });
+    await newUser.save();
+
+    return res.status(200).json({ message: "signup successfull!" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "provide all area" });
+  }
+  try {
+    const findUser = await User.findOne({ email });
+    if (!findUser) {
+      return res.status(400).json({ message: "user not found" });
+    }
+    const isPasswordMatch = await bcrypt.compare(password, findUser.password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({ message: "password doesn't match" });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: findUser._id,
+        email: findUser.email,
+        displayName: findUser.displayName,
+        username: findUser.username,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
     );
     if (!process.env.JWT_SECRET) {
-        return res.status(500).json({ message: "JWT_SECRET is not configured" });
+      return res.status(500).json({ message: "JWT_SECRET is not configured" });
     }
 
-    res.cookie("token",token,{
-        httpOnly:true,
-        secure:true,
-        maxAge:3600000
-    })
-    
-    return res.status(200).json({
-        userId: findUser._id,
-        email:findUser.email,
-        displayName:findUser.displayName,
-        username:findUser.username
-
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 3600000,
     });
-    } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message });
+
+    return res.status(200).json({
+      userId: findUser._id,
+      email: findUser.email,
+      displayName: findUser.displayName,
+      username: findUser.username,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+const getCurrentUser = async (req, res) => {
+  const user = req.user;
+
+  try {
+    if (!user) {
+      return res.status(400).json({ message: "user not found" });
+    }
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: "server error", error });
+  }
+};
+
+const logout = async (req, res) => {
+  res.clearCookie("token");
+  return res.status(200).json({ message: "logout successfull" });
+};
+
+const getFriends = async (req, res) => {
+  const { userId } = req.body;
+
+  if (!userId) {
+    return res.status(400).json({ message: "no user id" });
+  }
+  try {
+    const findUser = await User.findById(userId);
+
+    if (!findUser) {
+      return res.status(400).json({ messsage: "user not found" });
     }
 
-}
-
-const getCurrentUser = async(req,res)=>{
-    const user = req.user; 
-    
-    try {
-      if(!user){
-        return res.status(400).json({ message: "user not found" });
-      }
-      return res.status(200).json(user);
-    } catch (error) {
-      return res.status(500).json({ message: "server error", error });
-    }
-  
+    console.log(findUser.friends);
+  } catch (error) {
+    return res.status(500).json({ message: "server error" });
   }
+};
 
-  const logout = async(req,res)=>{
-    res.clearCookie("token");
-    return res.status(200).json({ message: "logout successfull" });
-  }
-
-module.exports ={
-    signup,
-    login,
-    getCurrentUser,
-    logout
-}
+module.exports = {
+  signup,
+  login,
+  getCurrentUser,
+  logout,
+  getFriends,
+};
--- backend/router/UserRouter.js
+++ backend/router/UserRouter.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 const AuthMiddleware = require("../middleware/AuthMiddleware")
-const {signup,login,getCurrentUser,logout} = require("../controller/UserController");
+const {signup,login,getCurrentUser,logout,getFriends} = require("../controller/UserController");
 
 
 router.post("/signup",signup)
 router.post("/login",login)
 router.get("/getcurrent",AuthMiddleware,getCurrentUser)
 router.delete("/logout",logout)
+router.get("/getfriends",getFriends)
 
 
 module.exports = router;
