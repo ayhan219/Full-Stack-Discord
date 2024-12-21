c1e2c1d92712ede9bdaef0b476a552e48adcb245
Ayhan <ayhan-games@hotmail.com>
2024-12-22 01:57:08 +0300

    model user updated

--- backend/model/User.js
+++ backend/model/User.js
@@ -47,6 +47,13 @@ const UserSchema = new mongoose.Schema({
             ref:"User",
             default:null
         }
+    ],
+    menuChat:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            default:null 
+        }
     ]
 
 });
