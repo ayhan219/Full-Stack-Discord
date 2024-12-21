bf0866be8a18420b43f47905aaf3a436be63f71c
Ayhan <ayhan-games@hotmail.com>
2024-12-22 00:40:47 +0300

    profile page updated

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,89 +1,129 @@
+import { useEffect, useState } from "react";
+import { useUserContext } from "../context/UserContext";
 
 const Profile = () => {
-    return (
-      <div className="w-full h-screen bg-[#313338] flex items-center justify-center">
-        <div className="w-[600px] h-[600px] bg-[#1E1F22] flex flex-col items-center rounded-lg">
-            <div className="w-full h-auto flex items-center justify-between p-8">
-                <div className="w-64 h-auto flex items-center gap-5 ">
-                <div className="w-20 h-20 rounded-full flex items-center justify-center bg-gray-800 ">
-                    <img className="w-16 h-16 rounded-full" src="https://i.pinimg.com/1200x/98/1d/6b/981d6b2e0ccb5e968a0618c8d47671da.jpg" alt="" />
-                </div>
-                <div className="text-white font-bold">
-                    <h2>Ayhan</h2>
-                </div>
-                </div>
-                <div className="w-40 h-8 text-white font-semibold text-sm rounded-md flex items-center justify-center bg-[#4752C4] ">
-                    <button>Edit User Profile</button>
-                </div>
-            </div>
-            <div className="w-[95%] h-[72%] bg-[#2B2D31] rounded-lg flex flex-col gap-3 justify-evenly   ">
-                <div className="w-full h-16 p-5 flex justify-between items-center">
-                    <div className="w-auto flex flex-col gap-3">
-                    <div className="text-[#A8BAC1] font-semibold">
-                        <p>DISPLAY NAME</p>
-                    </div>
-                    <div className="text-white">
-                        <h3>Ayhan</h3>
-                    </div>
-                    </div>
-                    <div className="w-auto h-full flex items-center">
-                        <button className="w-16 h-10 bg-gray-600 text-white rounded-sm ">Edit</button>
-                    </div>
-                </div>
+    const {user} = useUserContext();
+
+    const [userEmailWithPrivate,setUserEmailWithPrivate] = useState<string>("");
 
 
-                <div className="w-full h-16 p-5 flex justify-between items-center">
-                    <div className="w-auto flex flex-col gap-3">
-                    <div className="text-[#A8BAC1] font-semibold">
-                        <p>USERNAME</p>
-                    </div>
-                    <div className="text-white">
-                        <h3>Ayhan219</h3>
-                    </div>
-                    </div>
-                    <div className="w-auto h-full flex items-center">
-                        <button className="w-16 h-10 bg-gray-600 text-white rounded-sm ">Edit</button>
-                    </div>
-                </div>
+    const handleEmail = ()=>{
 
-                <div className="w-full h-16 p-5 flex justify-between items-center">
-                    <div className="w-auto flex flex-col gap-3">
-                    <div className="text-[#A8BAC1] font-semibold">
-                        <p>EMAIL</p>
-                    </div>
-                    <div className="text-white">
-                        <h3>*********@gmail.com</h3>
-                    </div>
-                    </div>
-                    <div className="w-auto h-full flex items-center">
-                        <button className="w-16 h-10 bg-gray-600 text-white rounded-sm ">Edit</button>
-                    </div>
-                </div>
+        if(!user?.email){
+            return;
+        }
 
-                <div className="w-full h-16 p-5 flex justify-between items-center">
-                    <div className="w-auto flex flex-col gap-3">
-                    <div className="text-[#A8BAC1] bold-semibold">
-                        <p>PHONE NUMBER</p>
-                    </div>
-                    <div className="text-white">
-                        <h3>********1413</h3>
-                    </div>
-                    </div>
-                    <div className="w-auto h-full flex items-center">
-                        <button className="w-16 h-10 bg-gray-600 text-white rounded-sm ">Edit</button>
-                    </div>
-                </div>
+        const index = user.email.indexOf("@")
+        const getPartOfEmail = user.email.slice(0,index);
+        const result = getPartOfEmail?.replace(/./g, "*");
 
+        const findAfterAt = user.email.slice(index,user?.email.length)
+        const finalValue = result.concat(findAfterAt)
+        setUserEmailWithPrivate(finalValue) 
+    }
 
-
-                
-            </div>
-
-        </div>
-        
+    useEffect(()=>{
+        if(user){
+        handleEmail();
+        }
+    },[user])
+  return (
+    <div className="w-full h-screen bg-[#313338] flex flex-col items-center p-10 gap-10 ">
+      <div className="w-full flex justify-center text-white font-bold text-2xl">
+        <h3>My Account</h3>
       </div>
-    )
-  }
-  
-  export default Profile
-  
+      <div className="w-[600px] h-[600px] bg-[#1E1F22] flex flex-col items-center rounded-lg">
+        <div className="w-full h-auto flex items-center justify-between p-8">
+          <div className="w-64 h-auto flex items-center gap-5 ">
+            <div className="w-20 h-20 rounded-full flex items-center justify-center bg-gray-800 ">
+              <img 
+                className="w-16 h-16 rounded-full"
+                src="https://i.pinimg.com/1200x/98/1d/6b/981d6b2e0ccb5e968a0618c8d47671da.jpg"
+                alt=""
+              />
+            </div>
+            <div className="text-white font-bold">
+              <h2>{user?.username}</h2>
+            </div>
+          </div>
+          <div className="w-40 h-8 text-white font-semibold text-sm rounded-md flex items-center justify-center bg-[#4752C4] ">
+            <button>Edit User Profile</button>
+          </div>
+        </div>
+        <div className="w-[95%] h-[72%] bg-[#2B2D31] rounded-lg flex flex-col gap-3 justify-evenly   ">
+          <div className="w-full h-16 p-5 flex justify-between items-center">
+            <div className="w-auto flex flex-col gap-3">
+              <div className="text-[#A8BAC1] font-semibold">
+                <p>DISPLAY NAME</p>
+              </div>
+              <div className="text-white">
+                <h3>{user?.displayName}</h3>
+              </div>
+            </div>
+            <div className="w-auto h-full flex items-center">
+              <button className="w-16 h-10 bg-gray-600 text-white rounded-sm ">
+                Edit
+              </button>
+            </div>
+          </div>
+
+          <div className="w-full h-16 p-5 flex justify-between items-center">
+            <div className="w-auto flex flex-col gap-3">
+              <div className="text-[#A8BAC1] font-semibold">
+                <p>USERNAME</p>
+              </div>
+              <div className="text-white">
+                <h3>{user?.username}</h3>
+              </div>
+            </div>
+            <div className="w-auto h-full flex items-center">
+              <button className="w-16 h-10 bg-gray-600 text-white rounded-sm ">
+                Edit
+              </button>
+            </div>
+          </div>
+
+          <div className="w-full h-16 p-5 flex justify-between items-center">
+            <div className="w-auto flex flex-col gap-3">
+              <div className="text-[#A8BAC1] font-semibold">
+                <p>EMAIL</p>
+              </div>
+              <div className="text-white">
+                <h3>{userEmailWithPrivate}</h3>
+              </div>
+            </div>
+            <div className="w-auto h-full flex items-center">
+              <button className="w-16 h-10 bg-gray-600 text-white rounded-sm ">
+                Edit
+              </button>
+            </div>
+          </div>
+
+          <div className="w-full h-16 p-5 flex justify-between items-center">
+            <div className="w-auto flex flex-col gap-3">
+              <div className="text-[#A8BAC1] bold-semibold">
+                <p>PHONE NUMBER</p>
+              </div>
+              <div className="text-white">
+                <h3>********1413</h3>
+              </div>
+            </div>
+            <div className="w-auto h-full flex items-center">
+              <button className="w-16 h-10 bg-gray-600 text-white rounded-sm ">
+                Edit
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full flex items-center text-white font-bold text-2xl flex-col gap-3 ">
+        <h3>Password And Authentication</h3>
+        <div className="w-40 h-8 text-white font-semibold text-sm rounded-md flex items-center justify-center bg-[#4752C4] ">
+          <button>Edit User Profile</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
