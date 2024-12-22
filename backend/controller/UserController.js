2b4f69b280766dbcad263d38b6bbd91d5afa3ba6
Ayhan <ayhan-games@hotmail.com>
2024-12-22 20:30:13 +0300

    usercontroller updated for updating profile picture api

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -245,6 +245,27 @@ const addToMenuChat = async(req,res)=>{
 }
 
 
+const uploadProfilePicture = async (req, res) => {
+  const { userId } = req.body;
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded!" });
+  }
+
+  const filePath = `/uploads/${req.file.filename}`;
+  try {
+    const user = await User.findByIdAndUpdate(userId, { profilePic: filePath }, { new: true });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json({
+      message: "Profile picture updated successfully!",
+      profilePic: filePath,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong", error });
+  }
+};
+
+
 
 
 module.exports = {
@@ -254,5 +275,6 @@ module.exports = {
   logout,
   addFriend,
   acceptOrDecline,
-  addToMenuChat
+  addToMenuChat,
+  uploadProfilePicture
 };
