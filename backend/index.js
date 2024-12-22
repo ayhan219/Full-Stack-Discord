b3989cdd9150b20a73a6215b9031a4f505f104b9
Ayhan <ayhan-games@hotmail.com>
2024-12-22 20:25:13 +0300

    multermiddleware and index.js updated

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const UserRoutes = require("./router/UserRouter");
 const ChannelRoutes = require("./router/ChannelRouter")
 const cookieParser = require("cookie-parser")
+const path = require("path");
 dotenv.config();
 
 
@@ -16,6 +17,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/auth",UserRoutes);
 app.use("/api/channel",ChannelRoutes)
 
