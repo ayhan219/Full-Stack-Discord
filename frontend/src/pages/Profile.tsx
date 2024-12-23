a0e0431f0521a7cbf8728d14bfe57e423f13bc75
Ayhan <ayhan-games@hotmail.com>
2024-12-23 18:39:53 +0300

    profile updated

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,32 +1,85 @@
 import { useEffect, useState } from "react";
 import { useUserContext } from "../context/UserContext";
+import axios from "axios";
+
+type User = {
+  userId: string;
+  email: string;
+  displayName: string;
+  username: string;
+  profilePic: string;
+  friends: Friend[];
+  pendingFriend: Friend[];
+  menuChat: Friend[];
+};
+
+interface Friend {
+  username: string;
+  _id: string;
+  profilePic: string;
+}
 
 const Profile = () => {
-    const {user} = useUserContext();
+  const { user,setUser } = useUserContext();
 
-    const [userEmailWithPrivate,setUserEmailWithPrivate] = useState<string>("");
+  const [userEmailWithPrivate, setUserEmailWithPrivate] = useState<string>("");
 
-
-    const handleEmail = ()=>{
-
-        if(!user?.email){
-            return;
-        }
-
-        const index = user.email.indexOf("@")
-        const getPartOfEmail = user.email.slice(0,index);
-        const result = getPartOfEmail?.replace(/./g, "*");
-
-        const findAfterAt = user.email.slice(index,user?.email.length)
-        const finalValue = result.concat(findAfterAt)
-        setUserEmailWithPrivate(finalValue) 
+  const handleEmail = () => {
+    if (!user?.email) {
+      return;
     }
 
-    useEffect(()=>{
-        if(user){
-        handleEmail();
+    const index = user.email.indexOf("@");
+    const getPartOfEmail = user.email.slice(0, index);
+    const result = getPartOfEmail?.replace(/./g, "*");
+
+    const findAfterAt = user.email.slice(index, user?.email.length);
+    const finalValue = result.concat(findAfterAt);
+    setUserEmailWithPrivate(finalValue);
+  };
+
+  useEffect(() => {
+    if (user) {
+      handleEmail();
+    }
+  }, [user]);
+
+  const handleProfileImageChange = async (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (!e.target.files || e.target.files.length === 0) {
+      return;
+    }
+
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("profilePic", file);
+    formData.append("userId",user?.userId || "")
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/auth/upload-profile",
+        formData,
+        {
+          headers: { "Content-Type": "multipart/form-data" },
         }
-    },[user])
+      );
+
+      if(response.status===200){
+        setUser((prev)=>{
+          if(!prev){
+            return prev;
+          }
+          return{
+            ...prev,
+            profilePic:response.data.profilePic
+          }
+        })
+      }
+    } catch (error) {
+      console.error("Error uploading profile picture:", error);
+    }
+  };
   return (
     <div className="w-full h-screen bg-[#313338] flex flex-col items-center p-10 gap-10 ">
       <div className="w-full flex justify-center text-white font-bold text-2xl">
@@ -35,13 +88,23 @@ const Profile = () => {
       <div className="w-[600px] h-[600px] bg-[#1E1F22] flex flex-col items-center rounded-lg">
         <div className="w-full h-auto flex items-center justify-between p-8">
           <div className="w-64 h-auto flex items-center gap-5 ">
-            <div className="w-20 h-20 rounded-full flex items-center justify-center bg-gray-800 ">
-              <img 
+            <div
+              className="w-20 h-20 rounded-full flex items-center justify-center bg-gray-800 cursor-pointer"
+              onClick={() => document.getElementById("fileInput")?.click()}
+            >
+              <img
                 className="w-16 h-16 rounded-full"
-                src="https://i.pinimg.com/1200x/98/1d/6b/981d6b2e0ccb5e968a0618c8d47671da.jpg"
+                src={`http://localhost:5000${user?.profilePic}`}
                 alt=""
               />
             </div>
+            <input
+              type="file"
+              id="fileInput"
+              className="hidden"
+              onChange={handleProfileImageChange}
+            />
+
             <div className="text-white font-bold">
               <h2>{user?.username}</h2>
             </div>
@@ -119,7 +182,7 @@ const Profile = () => {
       <div className="w-full flex items-center text-white font-bold text-2xl flex-col gap-3 ">
         <h3>Password And Authentication</h3>
         <div className="w-40 h-8 text-white font-semibold text-sm rounded-md flex items-center justify-center bg-[#4752C4] ">
-          <button>Edit User Profile</button>
+          <button>Edit Password</button>
         </div>
       </div>
     </div>
