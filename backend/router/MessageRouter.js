af19b3ce28a92191f338639f9161740e5294ce54
Ayhan <ayhan-games@hotmail.com>
2024-12-27 16:11:56 +0300

    messagerouter updated

--- backend/router/MessageRouter.js
+++ backend/router/MessageRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const {saveChat} = require("../controller/MessageController");
+const {saveChat,getMessages} = require("../controller/MessageController");
 
 
 router.post("/savechat",saveChat);
+router.get("/getmessages",getMessages)
 
 module.exports = router;
