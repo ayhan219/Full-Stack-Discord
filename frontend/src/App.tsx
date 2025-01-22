fe414e8b861c400b5350df6d9e735f469c848e07
Ayhan <ayhan-games@hotmail.com>
2025-01-22 22:35:41 +0300

    app.tsx updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ReactNode } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Sidebar from "./components/Sidebar";
@@ -10,44 +10,75 @@ import FriendChat from "./pages/FriendChat";
 import Profile from "./pages/Profile";
 import { LiveKitRoom, RoomAudioRenderer } from "@livekit/components-react";
 
+// PrivateRoute bileşeni
+interface PrivateRouteProps {
+  children: ReactNode;
+}
 
+const PrivateRoute = ({ children }: PrivateRouteProps) => {
+  const { user } = useUserContext();
+  return user ? <>{children}</> : <Navigate to="/login" replace />;
+};
 
 function App() {
-  const { user,token } = useUserContext();
+  const { user, token } = useUserContext();
   const serverUrl = "wss://discord-clone-6tnm5nqn.livekit.cloud";
 
   return (
     <BrowserRouter>
-    <LiveKitRoom
+      <LiveKitRoom
         video={true}
         audio={true}
-        connect={true}
+        connect={!!user}
         token={token}
         serverUrl={serverUrl}
         data-lk-theme="default"
         style={{ height: "100vh" }}
       >
         <RoomAudioRenderer />
-        
-      
-      {/* <div className='flex'>
-  
-  </div> */}
-      <div className="flex">
-        {user && (
-          <Sidebar
-          />
-        )}
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="/channel" element={<Channel />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/friendchat" element={<FriendChat />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </div>
+        <div className="flex">
+          {user && <Sidebar />}
+          <Routes>
+            {/* Genel Rotalar */}
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+
+            {/* Korunan Rotalar */}
+            <Route
+              path="/channel"
+              element={
+                <PrivateRoute>
+                  <Channel />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/home"
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/friendchat"
+              element={
+                <PrivateRoute>
+                  <FriendChat />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <PrivateRoute>
+                  <Profile />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </div>
       </LiveKitRoom>
     </BrowserRouter>
   );
