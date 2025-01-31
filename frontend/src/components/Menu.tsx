7cf3d4b509e810020f2efb59fa9db568f8215d4e
Ayhan <ayhan-games@hotmail.com>
2025-01-31 13:17:02 +0300

    menu updated

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -74,7 +74,7 @@ const Menu = ({activeMenu,setActiveMenu}:MenuProps ) => {
         <h3>DIRECT MESSAGES</h3>
         <GoPlus className=" text-xl cursor-pointer" />
         </div>
-        <div className="w-full h-[570px] flex flex-col gap-4 overflow-y-auto custom-scrollbar2 ">
+        <div className="w-full h-[570px] flex flex-col gap-4 overflow-y-auto scrollbar-hidden ">
           {
             user?.menuChat.map((item,index)=>(
               <MenuFriends key={index} item={item} setActiveMenuFriend={setActiveMenuFriend} activeMenuFriend={activeMenuFriend} />
