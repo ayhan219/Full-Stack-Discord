5c9b3951115611ae7498b230ad44e711cffb1638
Ayhan <ayhan-games@hotmail.com>
2025-02-05 03:54:18 +0300

    login and signup updated

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import dcbackgroung from "../assets/discordback.png";
 import axios from "axios";
 import { useUserContext } from "../context/UserContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {toast } from 'react-toastify';
 
 const Login = () => {
