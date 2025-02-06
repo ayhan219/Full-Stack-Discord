4686a091ba25f702175f51c674617d9128d8f29a
Ayhan Taha Pastutmaz <113910977+ayhan219@users.noreply.github.com>
2025-02-06 20:06:52 +0300

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:5173", 
+    origin: "https://full-stack-discord-frontend.onrender.com", 
     methods: ['GET', 'POST'],
   }
 });
