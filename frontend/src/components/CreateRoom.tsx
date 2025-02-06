acbf443904d57db561fd835acab3c8f1c2248d33
Ayhan <ayhan-games@hotmail.com>
2025-02-07 01:14:58 +0300

    createroom and createvoiceroom updated

--- frontend/src/components/CreateRoom.tsx
+++ frontend/src/components/CreateRoom.tsx
@@ -112,13 +112,6 @@ const CreateRoom = () => {
             Cancel
           </button>
           <button
-            onClick={() => handleAddChatRoom()}
-            disabled={isCreating}
-            className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-500"
-          >
-            Create
-          </button>
-          <button
             className={`px-4 py-2 text-sm font-medium text-white rounded-lg ${
               isCreating ? "bg-gray-500" : "bg-blue-600 hover:bg-blue-500"
             }`}
--- frontend/src/components/CreateVoiceRoom.tsx
+++ frontend/src/components/CreateVoiceRoom.tsx
@@ -115,13 +115,6 @@ const CreateVoiceRoom = () => {
             Cancel
           </button>
           <button
-          onClick={()=>handleAddVoiceRoom()}
-          disabled={isCreating}
-            className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-500"
-          >
-            Create
-          </button>
-          <button
             className={`px-4 py-2 text-sm font-medium text-white rounded-lg ${isCreating ? "bg-gray-500" : "bg-blue-600 hover:bg-blue-500"}`}
             onClick={handleAddVoiceRoom}
             disabled={isCreating}
